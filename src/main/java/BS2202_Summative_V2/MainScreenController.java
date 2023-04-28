e13b78b39212144fc74b6a3cff6b90052ddd204e
kacpergagatek <k.gagatek.21@unimail.winchester.ac.uk>
2023-04-28 23:22:21 +0100

    MainScreenController is now has functional buttons but it does not pass any information

--- src/main/java/BS2202_Summative_V2/MainScreenController.java
+++ src/main/java/BS2202_Summative_V2/MainScreenController.java
@@ -1,5 +1,47 @@
 package BS2202_Summative_V2;
 
-public class MainScreenController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+public class MainScreenController
+{
+    @FXML public Button loanNewItemButton;
+    @FXML public Button submitReviewButton;
+    @FXML public Button returnItemButton;
+
+
+    @FXML public void handleLoanNewItemButtonAction(ActionEvent event) throws Exception
+    {
+        Stage stage = (Stage) loanNewItemButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("loan_new_item_screen.fxml"));
+        Parent root = loader.load();
+        Scene changeScene = new Scene(root, 1512, 982);
+        stage.setScene(changeScene);
+        stage.show();
+    }
+
+    @FXML public void handleSubmitReviewButtonAction(ActionEvent event) throws Exception
+    {
+        Stage stage = (Stage) submitReviewButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("select_item_for_review.fxml"));
+        Parent root = loader.load();
+        Scene changeScene = new Scene(root, 1512, 982);
+        stage.setScene(changeScene);
+        stage.show();
+    }
+
+    @FXML public void handleReturnItemButtonAction(ActionEvent event) throws Exception
+    {
+        Stage stage = (Stage) returnItemButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("return_item_screen.fxml"));
+        Parent root = loader.load();
+        Scene changeScene = new Scene(root, 1512, 982);
+        stage.setScene(changeScene);
+        stage.show();
+    }
 }

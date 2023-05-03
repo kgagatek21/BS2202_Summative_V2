e6fde23e3f137d8f0ed07cd5a3d1a2584e4e1d02
kacpergagatek <k.gagatek.21@unimail.winchester.ac.uk>
2023-05-04 00:19:09 +0100

    started doing review controller

---
+++ src/main/java/BS2202_Summative_V2/SelectItemForReviewController.java
@@ -0,0 +1,53 @@
+package BS2202_Summative_V2;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SelectItemForReviewController implements Initializable
+{
+
+    public ImageView returnImageView;
+    public Button filterForBooksButton;
+    public Button filterForMoviesButton;
+    public Button filterForMusicButton;
+    public TextField searchTextField;
+    public Button continueButton;
+    public ListView<String> itemListView;
+
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        itemListView = new ListView<String>();
+//        itemListView.getItems().addAll("Item 1", "item 2", "item 3");
+
+
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "Single", "Double", "Suite", "Family App");
+        itemListView.setItems(items);
+
+        System.out.println("here");
+    }
+
+
+    public void handleFilterForBooksButtonAction(ActionEvent event) {
+    }
+
+    public void handleFilterForMoviesButtonAction(ActionEvent event) {
+    }
+
+    public void handleFilterForMusicButtonAction(ActionEvent event) {
+    }
+
+    public void handleContinueButtonAction(ActionEvent event) {
+        itemListView = new ListView<>();
+        itemListView.getItems().addAll("Item 1", "item 2", "item 3");
+    }
+}
